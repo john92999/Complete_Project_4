f0b1ba343a4d4ad8f42e682d45602bda40ecbd9c
pjwesley <pjwesley7@gmail.com>
2026-03-11 12:03:29 +0530

    added som new files

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "vpc_id" {
   value = module.vpc.vpc_id
+  sensitive = true
 }
 
 output "vpc_cidr_block" {
   value = module.vpc.vpc_cidr_block
+  sensitive = true
 }
 
 output "vpc_tags" {
   value = module.vpc.vpc_tags
+  sensitive = true
 }
--- provider.tf
+++ provider.tf
@@ -4,4 +4,5 @@ provider "aws" {
 
 provider "vault" {
   address = "http://10.48.17.203:8200"
+  token = "root"
 }
